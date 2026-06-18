346fb067b436de223e1815f6206969cd13bd62ab
SinghAnirudh18 <YOUR_GITHUB_EMAIL>
2026-06-19 03:22:22 +0530

    Fix Vercel base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
     plugins: [tailwindcss(), react()],
     server: {
       port: env.PORT ? parseInt(env.PORT) : 5173,
     },
-    base:process.env.VITE_BASE_PATH||"/codeflow"
-  };
-});
+    base: "/"
+  }
+})
